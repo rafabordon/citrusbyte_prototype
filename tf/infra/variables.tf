694415e2d8d3c7fd65bc48b87414018ee50711ff
Rafael Bordon <bordonrafael@gmail.com>
2018-01-08 16:18:31 -0300

    Adding gitignore and removing secrets from variables file

--- tf/infra/variables.tf
+++ tf/infra/variables.tf
@@ -24,11 +24,3 @@ variable  "availability_zones"  {
 variable  "pub_dest_cidr_blocks"  {
   default = "10.10.15.206/32,10.72.162.199/32,10.72.162.200/32,10.120.23.0/24,10.128.61.62/32,10.128.61.107/32,10.128.61.108/32"
 }
-
-variable  "access_key"  {
-  default = "AKIAJGWRCHBEBICP37RQ"
-}
-
-variable  "secret_key"  {
-  default = "82Lnw2ya43uYf5JIQo8OcbyIHyQNfsjsQ9+TeFhl" 
-}
